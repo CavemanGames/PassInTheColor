e0eccbc446211524cedbb7269b4180bb0688f685
twistereventos <luiz@twistereventos.com>
2017-09-08 00:48:52 -0300

    Create the project

---
+++ PassInTheColor/Source/PassInTheColor/PassInTheColor.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PassInTheColor.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PassInTheColor, "PassInTheColor" );
---
+++ PassInTheColor/Source/PassInTheColor/PassInTheColorGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PassInTheColorGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PASSINTHECOLOR_API APassInTheColorGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
